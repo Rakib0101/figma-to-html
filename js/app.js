8c0afa58ebbe984fbbf4cdc72b1e8036c4ce4d25
Rakib0101 <rakib.01jan@gmail.com>
2022-01-20 14:17:11 +0600

    Mobile menu and other all functionalities are completed now need to check final revision

--- js/app.js
+++ js/app.js
@@ -1,26 +1,35 @@
+const loadMore = document.querySelector("#load-more");
+
 function loadPosts() {
     fetch("https://rakib0101.github.io/blog_json/blog.json")
-        .then(res => res.json())
-        .then(data => {
-            displayTwoColPosts(data)
-            displayThreeColPosts(data)
-        })
+        .then((res) => res.json())
+        .then((data) => {
+            displayTwoColPosts(data);
+            displayThreeColPosts(data);
+        });
 }
-
+function loadDuePosts() {
+    
+    fetch("https://rakib0101.github.io/blog_json/blog.json")
+        .then((res) => res.json())
+        .then((data) => {
+            displayDuePosts(data);
+            loadMore.style.display = "none";
+        });
+}
 function displayTwoColPosts(posts) {
-    const slicePosts = posts.slice(0, 2)
-    const twoColPosts = document.getElementById('two-col-posts')
+    const slicePosts = posts.slice(0, 2);
+    const twoColPosts = document.getElementById("two-col-posts");
     for (const post of slicePosts) {
-        const div = document.createElement('div');
-        div.classList.add('blog-post')
+        const div = document.createElement("div");
+        div.classList.add("blog-post");
         div.innerHTML = `
             <img src=${post.feature_img} />
             <p class="blog-category">${post.blog_category}</p>
             <h2 class="blog-title">${post.blog_title}</h2>
             <p class="published-date">${post.published_date}</P>
         `;
-        twoColPosts.appendChild(div)
-        console.log(post);
+        twoColPosts.appendChild(div);
     }
 }
 function displayThreeColPosts(posts) {
@@ -36,7 +45,49 @@ function displayThreeColPosts(posts) {
             <p class="published-date">${post.published_date}</P>
         `;
         threeColPosts.appendChild(div);
+    }
+}
+function displayDuePosts(posts) {
+    const slicePosts = posts.slice(8, 12);
+    console.log(slicePosts);
+    const threeColPosts = document.getElementById("load-more-three-col-posts");
+    for (const post of slicePosts) {
+        const div = document.createElement("div");
+        div.classList.add("load-more-three-blog-post");
+        div.innerHTML = `
+            <img src=${post.feature_img} />
+            <p class="blog-category">${post.blog_category}</p>
+            <h2 class="blog-title">${post.blog_title}</h2>
+            <p class="published-date">${post.published_date}</P>
+        `;
+        threeColPosts.appendChild(div);
         console.log(post);
     }
 }
-loadPosts()
+loadPosts();
+
+//Menu
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
+
+function toggleMenu() {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove("showMenu");
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+        menuItems.style.visibility = "visible";
+    } else {
+        menu.classList.add("showMenu");
+        closeIcon.style.display = "block";
+        menuIcon.style.display = "none";
+    }
+}
+
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach(function (menuItem) {
+    menuItem.addEventListener("click", toggleMenu);
+});
